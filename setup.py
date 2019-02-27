62ee94532997b93e417e27cd78ecabce5df1cf17
dmroeder <dmroeder@gmail.com>
2019-02-27 09:32:14 -0800

    added setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="pylogix",
+    version="0.3.0",
+    author="Dustin Roeder",
+    author_email="dmroeder@gmail.com",
+    description="Read/Write Rockwell Automation Logix based PLC's",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    url="https://github.com/dmroeder/pylogix",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        ],
+    )
