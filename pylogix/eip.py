1ca6585569c358cf9c671307840c46eaac0cff72
Amber Snow <11779550+ambersnow@users.noreply.github.com>
2019-09-23 15:31:26 -0600

    #68 — fix GetTagList() AttributeError
    Line 127 is looking for a lowercase “value” when the Response object used by `self._getTagList` is a capitalized “Value”. Changing this to a capital V fixes it (from my testing) and allows it to find the Value to process into `self._getUDT`

--- pylogix/eip.py
+++ pylogix/eip.py
@@ -124,7 +124,7 @@ class PLC:
         otherwise controller tags and program tags.
         '''
         tag_list = self._getTagList(allTags)
-        updated_list = self._getUDT(tag_list.value)
+        updated_list = self._getUDT(tag_list.Value)
         
         return Response(None, updated_list, tag_list.status)
 
