5064e0943d165811e8d32027df148dd428d28967
Fernando Balandran <ic3balandran@yahoo.com>
2020-04-01 12:08:47 -0500

    documentation-01

--- docs/python_code/test-01.py
+++ docs/python_code/test-01.py
@@ -1,30 +1,12 @@
 from pylogix import PLC
-from ping3 import ping
-import sys
-
-
-def read_tag(tag):
-    return comm.Read(tag)
-
 
 # Setup the PLC object with initial parameters
-comm = PLC()
-controller_ip = '192.168.0.26'  # Change to your plc ip address
-controller_slot = 1  # Change to your plc slot number, Only do this if slot is not 0
-comm.IPAddress = controller_ip
-comm.ProcessorSlot = controller_slot
+# Change to your plc ip address, and slot, default is 0, shown for clarity
+comm = PLC('192.168.1.207', 0)
 
-# ensure plc is pingable, else exit the program
-if ping(controller_ip) is None:
-    print("Controller unreachable, check pc ip settings")
-    sys.exit()
+# Read returns Response class (.TagName, .Value, .Status)
+ret = comm.Read('bool_01')
+print(ret.TagName, ret.Value, ret.Status)
 
-# try to read a tag, else print error
-try:
-    # if you have another controller boolean tag, feel free to change it
-    value = read_tag('bool_01')
-    print(value)
-except NameError as e:
-    print(e)
-except ValueError as e:
-    print(e)
+# Close Open Connection to the PLC
+comm.Close()
--- docs/python_code/test-03.py
+++ docs/python_code/test-03.py
@@ -1,32 +1,14 @@
 from pylogix import PLC
-from ping3 import ping
-import sys
 import time
 
-
-def read_tag(tag):
-    return comm.Read(tag)
-
-
 # Setup the PLC object with initial parameters
-comm = PLC()
-controller_ip = '192.168.176.1'  # Change to your plc ip address
-controller_slot = 1  # Change to your plc slot number, Only do this if slot is not 0
-comm.IPAddress = controller_ip
-comm.ProcessorSlot = controller_slot
-
-# ensure plc is pingable, else exit the program
-if ping(controller_ip) is None:
-    print("Controller unreachable, check pc ip settings")
-    sys.exit()
+# Change to your plc ip address, and slot, default is 0, shown for clarity
+comm = PLC('192.168.1.207', 0)
 
 # try to read a tag, else print error
-try:
-    while True:
-        value = read_tag('bool_01')
-        time.sleep(1)  # Change seconds here
-        print(value)  # Do Ctrl + C to interrupt process
-except NameError as e:
-    print(e)
-except ValueError as e:
-    print(e)
+while True:
+    ret = comm.Read('bool_01')
+    time.sleep(1)  # Change seconds here
+    print(ret.Value)  # Do Ctrl + C to interrupt process
+
+comm.Close()
