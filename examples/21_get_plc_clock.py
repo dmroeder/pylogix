db6fe6a60fd3549a8512a1b6e489b83b1d7d22c8
Fernando B <fredz0003@gmail.com>
2020-05-04 21:20:56 -0500

    Update 21_get_plc_clock.py

--- examples/21_get_plc_clock.py
+++ examples/21_get_plc_clock.py
@@ -13,7 +13,19 @@ from pylogix import PLC
 
 with PLC() as comm:
     comm.IPAddress = '192.168.1.9'
-    ret = comm.GetPLCTime()
+    time_value = ret.Value
 
-    # print each pice of time
-    print(ret.TagName, ret.Value, ret.Status)
+    # print the Response value
+    print(ret)
+
+    # print the whole value
+    print(time_value)
+
+    # print each piece of time
+    print(time_value.year,
+          time_value.month,
+          time_value.day,
+          time_value.hour,
+          time_value.minute,
+          time_value.second,
+          time_value.microsecond)
