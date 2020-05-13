b48392174b4a84f97a2921e731852ab44aae87a0
Fernando Balandran <fredz0003@gmail.com>
2020-05-13 15:46:55 -0500

    little-endian

--- pylogix/eip.py
+++ pylogix/eip.py
@@ -63,20 +63,20 @@ class PLC:
         self.StructIdentifier = 0x0fCE
         self.StringEncoding = 'utf-8'
         self.CIPTypes = {0: (0, "UNKNOWN", '?'),
-                         160: (88, "STRUCT", 'B'),
+                         160: (88, "STRUCT", '<B'),
                          193: (1, "BOOL", '?'),
-                         194: (1, "SINT", 'b'),
-                         195: (2, "INT", 'h'),
-                         196: (4, "DINT", 'i'),
-                         197: (8, "LINT", 'q'),
-                         198: (1, "USINT", 'B'),
-                         199: (2, "UINT", 'H'),
-                         200: (4, "UDINT", 'I'),
-                         201: (8, "LWORD", 'Q'),
-                         202: (4, "REAL", 'f'),
-                         203: (8, "LREAL", 'd'),
-                         211: (4, "DWORD", 'I'),
-                         218: (1, "STRING", 'B')}
+                         194: (1, "SINT", '<b'),
+                         195: (2, "INT", '<h'),
+                         196: (4, "DINT", '<i'),
+                         197: (8, "LINT", '<q'),
+                         198: (1, "USINT", '<B'),
+                         199: (2, "UINT", '<H'),
+                         200: (4, "UDINT", '<I'),
+                         201: (8, "LWORD", '<Q'),
+                         202: (4, "REAL", '<f'),
+                         203: (8, "LREAL", '<d'),
+                         211: (4, "DWORD", '<I'),
+                         218: (1, "STRING", '<B')}
 
     def __enter__(self):
         return self
