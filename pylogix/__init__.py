31cb3c1f0db230b6abefd1462a61b41293ad2165
dmroeder <dmroeder@gmail.com>
2020-09-02 21:22:38 -0700

    Upped version to 0.7.3

--- pylogix/__init__.py
+++ pylogix/__init__.py
@@ -1,4 +1,3 @@
-#from .lgxDevice import LGXDevice
-from .eip import PLC
-__version_info__ = (0, 7, 2)
+from eip import PLC
+__version_info__ = (0, 7, 3)
 __version__ = '.'.join(str(x) for x in __version_info__)
