ba0812886d07b0435d7ca74d763d9684ea45eb91
Fernando B <fernandobe+git@protonmail.com>
2022-12-27 15:47:41 -0600

    minor-changes

--- pylogix/lgx_uvendors.py
+++ pylogix/lgx_uvendors.py
@@ -1,21 +1,24 @@
-### Micropython proxy class for large dict vendors in lgx_vendors.py
+# Micropython proxy class for large dict vendors in lgx_vendors.py
 class UVENDORS:
-    ### Look up on the fly without loading all vendor data into memory
-    ### .__getitem__ method is called when evaluating `uvendors[id]`
-    def __getitem__(self,vendorID):
+    # Look up on the fly without loading all vendor data into memory
+    # .__getitem__ method is called when evaluating `uvendors[id]`
+    def __getitem__(self, vendorID):
         sID = str(vendorID)
-        with open(__file__+".txt") as vendor_data:
+        with open(__file__ + ".txt") as vendor_data:
             for line in vendor_data:
                 k, v = line.split(':')
                 if k == sID: return v.strip()
         return 'Unknown'
 
-    ### Do-nothing on class initialization
-    def __init__(self): pass
+    # Do-nothing on class initialization
+    def __init__(self):
+        pass
 
-    ### Test if vendorID is in file of vendor IDs
-    ### .__contains__ method is called when executing `id in uvendors`
-    ### - use .__getitem__ method above
-    def __contains__(self,vendorID): return self[vendorID]!='Unknown'
+    # Test if vendorID is in file of vendor IDs
+    # .__contains__ method is called when executing `id in uvendors`
+    # - use .__getitem__ method above
+    def __contains__(self, vendorID):
+        return self[vendorID] != 'Unknown'
+
 
 uvendors = UVENDORS()
