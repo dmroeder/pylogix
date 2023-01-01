73e4e298fedaf4c82da3e9f1b8a2e056924fd055
Brian Carcich <briantcarcich@gmail.com>
2022-12-31 22:57:39 -0500

    Use binary search for lgx_uvendors Rename some files Fix symlink Add timing tests to build_mpy.sh

--- build_mpy.sh
+++ build_mpy.sh
@@ -9,3 +9,35 @@ mpy-cross pylogix/lgx_response.py -o upylogix/lgx_response.mpy
 mpy-cross pylogix/lgx_tag.py -o upylogix/lgx_tag.mpy
 mpy-cross pylogix/utils.py -o upylogix/utils.mpy
 mpy-cross pylogix/lgx_uvendors.py -o upylogix/lgx_uvendors.mpy
+
+python << EoFPython
+from pylogix.lgx_vendors import vendors as vdict
+vlist = list()
+ks = sorted(vdict.keys())
+maxL = 0
+
+for k in ks:
+    assert isinstance(k,int)
+    v = vdict[k]
+    assert isinstance(v,str)
+    vencoded = "{0}:{1}".format(k,v).encode('UTF-8')
+    vlist.append(vencoded)
+    L = len(vencoded)
+    if L > maxL: maxL = L
+
+newline = b'\n'
+maxspaces = b' ' * maxL
+
+oneline = lambda v: (v + maxspaces)[:maxL] + newline
+
+with open('upylogix/vendors.bin','wb') as fbin:
+    headerstring = "{0}:{1}".format(len(ks),"Count of vendors")
+    fbin.write(oneline(headerstring.encode('UTF-8')))
+    for bin in vlist: fbin.write(oneline(bin))
+EoFPython
+
+time micropython -c 'import pylogix.lgx_vendors as v;[None for k in v.vendors if None is v.vendors[k]]'
+time micropython -c 'import pylogix.lgx_vendors as v;import pylogix.lgx_uvendors as u;[None for k in v.vendors if None is u.uvendors.getitem_O_N(k)]'
+time micropython -c 'import pylogix.lgx_vendors as v;import pylogix.lgx_uvendors as u;[None for k in v.vendors if None is u.uvendors.getitem_O_logN(k)]'
+time micropython -c 'import pylogix.lgx_vendors as v;import pylogix.lgx_uvendors as u;[None for k in v.vendors if None is u.uvendors[k]]'
+
--- pylogix/lgx_uvendors.py
+++ pylogix/lgx_uvendors.py
@@ -3,12 +3,67 @@ class UVENDORS:
     # Look up on the fly without loading all vendor data into memory
     # .__getitem__ method is called when evaluating `uvendors[id]`
     def __getitem__(self, vendorID):
+        return self.getitem_O_logN(vendorID)
+
+    def getitem_O_logN(self, vendorID):
+
+        bnl,bcln = b'\n',b':'
+        fc = lambda bites: bites.index(bcln)
+        vID = lambda bites: bites[:fc(bites)]
+        idval = lambda lyne: int(vID(lyne))
+
+        with open(__file__ + ".bin",'rb') as vendor_file:
+
+            # Read and validate header line (through first newline),
+            # parse number of records, get fixed record length
+            hdr = vendor_file.readline()
+            assert bnl == hdr[-1:]
+            assert b':Count of vendors' == hdr[fc(hdr):].strip()
+            span = idval(hdr)
+            lo,hi,fixedlength = 1, 1+span, len(hdr)
+
+            # Read and validate first data line, i.e. next line
+            lin1 = vendor_file.readline()
+            assert len(lin1) == fixedlength
+            assert bnl == lin1[-1:]
+
+            # Parse vendor ID from first data line, check for match
+            idval1 = idval(lin1)
+            if idval1 == vendorID:
+                return lin1[fc(lin1)+1:].strip().decode('UTF-8')
+            elif idval1 > vendorID:
+                # Short-circuit the binary search if invariant fails
+                span = 0
+
+            # Binary search; invariant is vID(line@lo) < vendorID
+            while span > 1:
+                mid = lo + (span >> 1)
+                seek = (mid*fixedlength) - 1
+                assert vendor_file.seek(seek,0) == seek
+                lread = vendor_file.read(fixedlength+1)
+                assert bnl == lread[:1]
+                assert bnl == lread[-1:]
+                iID = idval(lread[1:])
+                if iID == vendorID:
+                    return lread[fc(lread)+1:].strip().decode('UTF-8')
+                elif iID < vendorID: lo = mid
+                else               : hi = mid
+                span = hi - lo
+            return 'Unknown'
+
+
+    # Obsolete O(N) lookup, replaced by O(logN) lookup above
+    def getitem_O_N(self, vendorID):
         sID = str(vendorID)
         with open(__file__ + ".txt") as vendor_data:
             for line in vendor_data:
-                # Ensure list of exactly two elements is assigned to k,v
-                k, v = (line.split(':') + ['Unknown'])[:2]
-                if k == sID: return v.strip()
+                # i) Split line into colon-separated tokens
+                # ii) If first token does not match vendorID, continue
+                # iii) Else Re-join remaining tokens and return
+                tokens = line.split(':')
+                if sID != tokens.pop(0): continue
+                return ':'.join(tokens)
+        # No tokens matched vendorID, return unknown result
         return 'Unknown'
 
     # Do-nothing on class initialization
