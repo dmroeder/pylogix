989dc97f487befdbb997c04de733e1ff363d919a
Brian Carcich <briantcarcich@gmail.com>
2023-01-02 10:19:36 -0500

    Extract vendor.bin-writing code from shell script

--- build_mpy.sh
+++ build_mpy.sh
@@ -10,31 +10,7 @@ mpy-cross pylogix/lgx_tag.py -o upylogix/lgx_tag.mpy
 mpy-cross pylogix/utils.py -o upylogix/utils.mpy
 mpy-cross pylogix/lgx_uvendors.py -o upylogix/lgx_uvendors.mpy
 
-python << EoFPython
-from pylogix.lgx_vendors import vendors as vdict
-vlist = list()
-ks = sorted(vdict.keys())
-maxL = 0
-
-for k in ks:
-    assert isinstance(k,int)
-    v = vdict[k]
-    assert isinstance(v,str)
-    vencoded = "{0}:{1}".format(k,v).encode('UTF-8')
-    vlist.append(vencoded)
-    L = len(vencoded)
-    if L > maxL: maxL = L
-
-newline = b'\n'
-maxspaces = b' ' * maxL
-
-oneline = lambda v: (v + maxspaces)[:maxL] + newline
-
-with open('upylogix/vendors.bin','wb') as fbin:
-    headerstring = "{0}:{1}".format(len(ks),"Count of vendors")
-    fbin.write(oneline(headerstring.encode('UTF-8')))
-    for bin in vlist: fbin.write(oneline(bin))
-EoFPython
+python build_vendor_bin.py
 
 time micropython -c 'import pylogix.lgx_vendors as v;import pylogix.lgx_uvendors as u;print([None for k in v.vendors if "Unknown" is v.vendors[k]])'
 #time micropython -c 'import pylogix.lgx_vendors as v;import pylogix.lgx_uvendors as u;print([None for k in v.vendors if "Unknown" is u.uvendors.getitem_O_N(k)])'
